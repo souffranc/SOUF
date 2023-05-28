c673ae67f67081f2a18d0db1aec9d4d81a6a46f1
souffranc <sasha.sizov.11@yandex.ru>
2023-05-28 20:58:35 +0700

    3
    добавили юсинг стд и хело ворд

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
+    cout << "Helo, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+
     return 0;
 }
